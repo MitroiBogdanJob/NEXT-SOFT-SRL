64b489d80634326efb8422ee2c05ed3ecd7f1b92
mitroi.bogdan.constantin93@gmail.com <mitroi.bogdan.constantin93@gmail.com>
2025-01-04 00:55:49 +0200

    Home Page - Text Correction

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -6,13 +6,15 @@ function HomePage() {
 			<h6>Your Trusted Partner in Web Solutions</h6>
 			<p class="text-large homepage-text-large_homepage-text-lg__24_gZ">
 				At <strong class="strong-font-normal highlight">NEXT SOFT</strong>, we
-				bring your ideas to life through expert
-				<strong class="strong-font-normal">development</strong> and design
-				services. We specialize in{" "}
-				<strong class="strong-font-normal">desktop applications</strong>,
-				<strong class="strong-font-normal">web applications</strong>, ERP
-				systems and <strong class="strong-font-normal">APIs</strong>. We are
-				committed to building long-term partnerships that evolve with your{" "}
+				bring your ideas to life through expert development and design services.
+				<br />
+				We specialize in{" "}
+				<strong class="strong-font-normal">Desktop applications, </strong>
+				<strong class="strong-font-normal">
+					WEB applications, ERP systems
+				</strong>
+				<strong class="strong-font-normal"> and APIs</strong>. We are committed
+				to building long-term partnerships that evolve with your{" "}
 				<strong class="strong-font-normal">business needs</strong>. We provide
 				impactful, scalable, and{" "}
 				<strong class="strong-font-normal">
