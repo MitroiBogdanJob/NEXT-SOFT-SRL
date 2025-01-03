68373b4b9085dc32385d9f2894d1b4dad2a1b59a
mitroi.bogdan.constantin93@gmail.com <mitroi.bogdan.constantin93@gmail.com>
2025-01-04 00:41:49 +0200

    FIRST COMMIT

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  // Adaugă acest import
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom"; // Adaugă acest import
+import "./Header.css";
+import logo from "../Logo/Next Soft LOGO.png";
 
 function Header() {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/contacttt">Contact</Link></li>
-                    <li><Link to="/Servici">Services</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+	return (
+		<header className="header-container">
+			{/* Adăugăm logo-ul */}
+			<img src={logo} alt="Logo" className="logo" />
+
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About Us</Link>
+					</li>
+					<li>
+						<Link to="/contacttt">Contact</Link>
+					</li>
+					<li>
+						<Link to="/Servici">Services</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,26 +1,28 @@
-import './HomePage.css';
+import "./HomePage.css";
 
-function HomePage()
-{
-   
-    return (
-<div className="menu-contain-text-left">
-   <h1>Your Trusted Partner in Web Solutions</h1> 
-   <p class="text-large homepage-text-large_homepage-text-lg__24_gZ">
-      At <strong class="strong-font-normal highlight">NEXT SOFT</strong>, we bring your ideas to life through expert 
-      <strong class="strong-font-normal">development</strong> and design services. We specialize in <strong class="strong-font-normal">desktop applications</strong>, 
-      <strong class="strong-font-normal">web applications</strong>, ERP systems, and <strong class="strong-font-normal">APIs</strong>. We are committed to building 
-      long-term partnerships that evolve with your <strong class="strong-font-normal">business needs</strong>. 
-      We provide impactful, scalable, and <strong class="strong-font-normal">user-friendly digital products</strong> 
-      that drive your <strong class="strong-font-normal">business growth</strong> globally.
-   </p>
-</div>
-
-
-
-
-    );
-
+function HomePage() {
+	return (
+		<div className="menu-contain-text-left">
+			<h6>Your Trusted Partner in Web Solutions</h6>
+			<p class="text-large homepage-text-large_homepage-text-lg__24_gZ">
+				At <strong class="strong-font-normal highlight">NEXT SOFT</strong>, we
+				bring your ideas to life through expert
+				<strong class="strong-font-normal">development</strong> and design
+				services. We specialize in{" "}
+				<strong class="strong-font-normal">desktop applications</strong>,
+				<strong class="strong-font-normal">web applications</strong>, ERP
+				systems and <strong class="strong-font-normal">APIs</strong>. We are
+				committed to building long-term partnerships that evolve with your{" "}
+				<strong class="strong-font-normal">business needs</strong>. We provide
+				impactful, scalable, and{" "}
+				<strong class="strong-font-normal">
+					user-friendly digital products
+				</strong>
+				that drive your{" "}
+				<strong class="strong-font-normal">business growth</strong> globally.
+			</p>
+		</div>
+	);
 }
 
 export default HomePage;
