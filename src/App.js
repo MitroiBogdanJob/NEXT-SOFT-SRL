09e8d05e2d23583ac5976dafc6bb9d8af9c7d7a3
mitroi.bogdan.constantin93@gmail.com <mitroi.bogdan.constantin93@gmail.com>
2025-01-10 01:05:51 +0200

    contact-form commit 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import React from "react";
 import Header from "./Shared.Header-Footer/Header/Header";
 import Footer from "./Shared.Header-Footer/Footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import AboutPage from "./AboutPage/AboutPage";
 import ContactPage from "./ContactPage/ContactPage";
@@ -15,7 +15,6 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/Form-Contact" element={<FormContact />} />
-
 				<Route path="/About" element={<AboutPage />} />
 				<Route path="/Contact" element={<ContactPage />} />
 				<Route path="/Services" element={<ServicesPage />} />
--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,19 +1,19 @@
 // FormContact.jsx
 import React from "react";
-import "./ContactForm.css"; // Importă fișierul CSS pentru stiluri
-
+import "./ContactForm.css"; 
+import sphere from "../Images/Sphere/sphere.webp";
 function ContactForm() {
 	return (
 		<>
-			<h2>Book a Meeting!</h2>
-			<img
-				className="rotating-img"
-				alt="sphere_6_jnpihb"
-				src="https://res.cloudinary.com/norabytes/image/upload/c_limit,w_3840/f_auto/q_50/v1/public/design/3d_holo/spheres/sphere_6_jnpihb"
+			<h6>Book a Meeting!</h6>
+
+			<img className="rotating-img" alt="sphere-Image" 
+			src={sphere} 
 			/>
+
 			<form>
+				<label>First Name </label>
 				<input type="text" id="fullname" name="fullname" />
-				<label>aaaaaaaaa</label>
 			</form>
 		</>
 	);
