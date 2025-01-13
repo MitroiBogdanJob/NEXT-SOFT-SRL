d954f7255bdbabc61355030c8b5de5e676e3f6fd
mitroi.bogdan.constantin93@gmail.com <mitroi.bogdan.constantin93@gmail.com>
2025-01-13 04:20:41 +0200

    commit 11 - 13.02.2025

--- src/Shared.Header-Footer/Footer/Footer.jsx
+++ src/Shared.Header-Footer/Footer/Footer.jsx
@@ -1,11 +1,35 @@
-import './Footer.css';
+import React from "react";
+import { Link } from "react-router-dom"; // Dacă folosești React Router pentru navigare
+import "./Footer.css"; // Importă fișierul CSS
 
-function Footer() {
-    return (
-        <footer className="footer">
-            <p>&copy; 2024 Next Soft SRL. All rights reserved.</p>
-        </footer>
-    );
-}
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="footer-content">
+				{/* Linkuri utile */}
+				<div className="footer-column">
+					<h4>Linkuri utile</h4>
+					<ul className="footer-list">
+						<li>
+							<Link to="/politica-de-confidentialitate" className="footer-link">
+								Politica de Confidențialitate
+							</Link>
+						</li>
+						<li>
+							<Link to="/termeni-si-conditii" className="footer-link">
+								Termeni și Condiții
+							</Link>
+						</li>
+					</ul>
+				</div>
+
+				{/* Copyright */}
+				<div className="footer-copyright">
+					<p>&copy; 2025 Next Soft SRL. Toate drepturile rezervate.</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
 export default Footer;
