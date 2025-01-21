c48ef77a43df5a7521e709b03e23c7e0ed4eebc9
mitroi.bogdan.constantin93@gmail.com <mitroi.bogdan.constantin93@gmail.com>
2025-01-21 17:31:05 +0200

    Mobile version(w.i.p. v1.0)  -task nr#3121

--- src/Shared.Header-Footer/Header/Header.js
+++ src/Shared.Header-Footer/Header/Header.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Adaugă acest import
 import "./Header.css";
 import logo from "../../Images/Logo/Next Soft LOGO.png";
 
 function Header() {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
+
 	return (
 		<header className="header-container">
 			{/* Adăugăm logo-ul */}
@@ -11,19 +17,38 @@ function Header() {
 				<img src={logo} alt="Logo" className="logo" />
 			</Link>
 
-			<nav>
+			{/* Butonul pentru meniul de mobil */}
+			<div
+				className={`burger-menu ${isMobileMenuOpen ? "open" : ""}`}
+				onClick={toggleMobileMenu}
+			>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+
+			{/* Meniul de navigare */}
+			<nav className={`nav-menu ${isMobileMenuOpen ? "open" : ""}`}>
 				<ul>
 					<li>
-						<Link to="/">Home</Link>
+						<Link to="/" onClick={() => setIsMobileMenuOpen(false)}>
+							Home
+						</Link>
 					</li>
 					<li>
-						<Link to="/AboutUs">About us</Link>
+						<Link to="/AboutUs" onClick={() => setIsMobileMenuOpen(false)}>
+							About us
+						</Link>
 					</li>
 					<li>
-						<Link to="/Services">Services</Link>
+						<Link to="/Services" onClick={() => setIsMobileMenuOpen(false)}>
+							Services
+						</Link>
 					</li>
 					<li>
-						<Link to="/ContactUs">Contact</Link>
+						<Link to="/ContactUs" onClick={() => setIsMobileMenuOpen(false)}>
+							Contact
+						</Link>
 					</li>
 				</ul>
 			</nav>
